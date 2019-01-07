258456c13101426c698415fc6f6d528859020c6c
rajesh kumar <rajesh@rajeshs-MacBook-Air.local>
2019-01-07 18:55:46 +0530

    buck code from DrKoz/CompMechCode

---
+++ script.py
@@ -0,0 +1,9 @@
+#!echo "hello python"
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib 
+plt.rc('font', family='CMU Serif',)
+
+matplotlib.rcParams['text.latex.preamble'] = [r'\boldmath']
