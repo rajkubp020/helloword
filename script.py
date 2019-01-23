1db954e30766a33e23f8b3a9002bf5792c068c55
rajesh kumar <rajesh@rajeshs-MacBook-Air.local>
2019-01-23 19:03:53 +0530

    python_2019_01_23

--- script.py
+++ script.py
@@ -1,9 +1,8 @@
 #!echo "hello python"
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib 
-plt.rc('font', family='CMU Serif',)
-
 matplotlib.rcParams['text.latex.preamble'] = [r'\boldmath']
+import sys
+sys.path.append("/Users/rajesh/work/github/python_packages")
